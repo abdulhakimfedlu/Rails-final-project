17b26dbd2c553f501aa9bd68db7178b9df9dcadb
jabezinu <jabezzinu31@gmail.com>
2025-07-30 23:24:36 +0300

    Global error handling

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
-end
+    # Global error handling
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+    
+    private
+    
+    def record_not_found(exception)
+      render json: { message: 'Record not found' }, status: :not_found
+    end
+    
+    def record_invalid(exception)
+      render json: { errors: exception.record.errors }, status: :unprocessable_entity
+    end
+  end
+  
